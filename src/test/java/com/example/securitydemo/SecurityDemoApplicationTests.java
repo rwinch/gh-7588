ef8dee27e257a3f5c9dfd280f79036797b1b6811
Rob Winch <rwinch@users.noreply.github.com>
2019-11-01 09:34:36 -0500

    add test

--- src/test/java/com/example/securitydemo/SecurityDemoApplicationTests.java
+++ src/test/java/com/example/securitydemo/SecurityDemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.securitydemo;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecurityDemoApplicationTests {
+	@Autowired
+	IDemoService service;
 
 	@Test
-	void contextLoads() {
+	void shouldBeSecure() {
+		assertThatCode(() -> service.methodWithParameter(new DemoEntity()))
+			.isNotNull();
 	}
 
 }
